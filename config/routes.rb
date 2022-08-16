041a57e238d432741f3ca36d8f1a413a07d4d9e3
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-08-16 11:14:27 +0100

    Make use of Scaffold

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'pages#index'
+end
